fcd964170d6e1c1c0a0b72a7da4048d14017e9a8
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-02-06 19:49:39 -0300

    Fim por hoje

--- js/carousel-main-page.js
+++ js/carousel-main-page.js
@@ -1,6 +1,15 @@
 $(document).ready(() => {
   $(".carousel-list__main-page").slick({
       slidesToShow: 4,
-      slidesToScroll: 4
+      slidesToScroll: 4,
+      // responsive: [
+      //   {
+      //     breakpoint: 1440,
+      //     settings: {
+      //       slidesToShow: 3,
+      //       slidesToScroll: 3
+      //     }
+      //   },
+      // ],
   });
 });
